308d3821d1e0daeb6a4e43f9ec1d82b62dee84ad
Matal1x <aslaoui.mohamed@gmail.com>
2023-03-12 22:16:04 +0100

    uri_applyer

--- Backend/GraphFunc/gfunc.py
+++ Backend/GraphFunc/gfunc.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rdflib import Graph as RDFGraph
 from rdflib import Literal, Namespace, URIRef
 from rdflib.extras.external_graph_libs import rdflib_to_networkx_graph
+
 # Create your views here.
 import os
 
@@ -17,11 +18,20 @@ g.parse("static\\rdf\\main.ttl")
 ex = Namespace("http://example.org/")
 g.bind("ex", ex)
 
-
-def graph_exist(S=None, P=None, O=None):
+def validator(S):
+    if S is not None:
+        return URIRef(ex+S)
+    
+    
+    
+def graph_exist(S, P, O):
     """
     returns a boolean value, check if a Subject, Predicat, Object or a combination of those exists
     """
+    
+    S=validator(S)
+    P=validator(P)
+    
     return ((S, P, O) in g)
 
 
@@ -33,6 +43,10 @@ def get_graph(S=None, P=None, O=None):
     temp = RDFGraph()
     ex = Namespace("http://example.org/")
     temp.bind("ex", ex)
+    
+    S=validator(S)
+    P=validator(P)
+    
     temp += g.triples((S, P, O))
     temp.serialize(
         destination=temprdfpath, format="turtle")
@@ -43,6 +57,10 @@ def delete_graph(S, P, O):
     beware not to put all values as empty or else it will remove the whole graph
     don't worry if you do so, you can regenerate it with the anycsvtordf.py file
     """
+    
+    S=validator(S)
+    P=validator(P)
+    
     g.remove((S, P, O))
     g.serialize(destination=mainrdfpath, format="turtle")
     print("Deletion successful!")
@@ -53,6 +71,10 @@ def add_graph(S, P, O):
     ALL elements must be specified
     if triple already exists, it will just be ignored and won't be duplicated
     """
+    
+    S=validator(S)
+    P=validator(P)
+    
     g.add((S, P, O))
     g.serialize(destination=mainrdfpath, format="turtle")
     print("Addition successful!")
@@ -62,14 +84,18 @@ def modify_graph(S, P, O):
     """
     ALL elements must be specified
     """
+    
+    S=validator(S)
+    P=validator(P)
+    
     g.remove((S, P, None))
     g.add((S, P, O))
     g.serialize(destination=mainrdfpath, format="turtle")
     print("Modification successful!")
 
 
-# print(graph_exist(URIRef(ex+"1"), URIRef(ex+"SourceIP"), None))                       WORKS!
-# get_graph(None, URIRef(ex+"SourceIP"), None)                                          WORKS!
+print(graph_exist("11", None, None))                       
+get_graph(None, "DestinationIP", None)                                         
 # delete_graph()                                                                        WORKS!
 # add_graph(URIRef(ex+"10"), URIRef(ex+"TimeStamp"), Literal("10/03/2023 23:40"))       WORKS!
 # modify_graph(URIRef(ex+"10"), URIRef(ex+"TimeStamp"),Literal("10/03/2023 23:57"))      WORKS!
