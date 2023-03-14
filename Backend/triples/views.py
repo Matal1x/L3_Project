d43f9c51907150952e7c15ebfb4547263b6c3b02
Matal1x <aslaoui.mohamed@gmail.com>
2023-03-14 23:04:36 +0100

    awaiting rdf presentation

--- Backend/triples/views.py
+++ Backend/triples/views.py
@@ -8,10 +8,11 @@ from GraphFunc import gfunc
 
 
 def index(request):
-        return render(request,'templates/generatedgraphs/index.html')
+        return render(request,'templates/triples/index.html')
 
 def form_name_view(request):
         form=TripleForm() #instance of a form
+        checked = False
         if request.method == 'POST':
             form= TripleForm(request.POST)
             if form.is_valid():
@@ -24,7 +25,8 @@ def form_name_view(request):
                       form.cleaned_data['objectV']=None
                 
                 gfunc.get_graph(form.cleaned_data['subject'], form.cleaned_data['predicat'], form.cleaned_data['objectV'])
+                checked = True
 
 
-        return render(request,'templates/generatedgraphs/formpage.html',{'form':form}) #request,pass in th page i want to show,pass in the context dictionary: we'll give it the key form and we'll pass in that actual form object 
+        return render(request,'templates/triples/formpage.html',{'form':form, 'checked': checked}) #request,pass in th page i want to show,pass in the context dictionary: we'll give it the key form and we'll pass in that actual form object 
 
