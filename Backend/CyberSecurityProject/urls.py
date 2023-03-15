0f08b9ed447250ea1af34d6c337a7dd796230341
ikraaama <abbasikram620@gmail.com>
2023-03-15 15:00:53 +0100

    helloo

--- Backend/CyberSecurityProject/urls.py
+++ Backend/CyberSecurityProject/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from triples import views
+from triples.views import MainGraphView
 
 urlpatterns = [
-    path('', views.index, name='index'), #homepage
+    path('', views.index, name='index'),#homepage
+      
     path('admin/', admin.site.urls), #admin site
     path('formpage/',views.form_name_view, name='form_name'),
     
--- Backend/triples/views.py
+++ Backend/triples/views.py
@@ -2,15 +2,23 @@ from django.shortcuts import render
 from .models import Triple
 from .forms import TripleForm
 from GraphFunc import gfunc
+from django.views.generic import View,TemplateView
 # Create your views here.
+
+class MainGraphView(TemplateView):
+      template_name='generatedgraphs/main.ttl'
+
+
 def index(request):
         return render(request,'templates/triples/index.html')
 
 def form_name_view(request):
         form=TripleForm() #instance of a form
         checked = False
+        graph=gfunc.get_graph()
         if request.method == 'POST':
             form= TripleForm(request.POST)
+            graph=gfunc.get_graph()
             if form.is_valid():
                 print("validated data")
                 if form.cleaned_data['subject'] == "":
@@ -24,7 +32,12 @@ def form_name_view(request):
                 print("objectV:", form.cleaned_data['objectV'])
                 
                 gfunc.get_graph(form.cleaned_data['subject'], form.cleaned_data['predicat'], form.cleaned_data['objectV'])
-                checked = True
-        return render(request,'templates/triples/formpage.html',{'form':form, 'checked': checked}) #request,pass in th page i want to show,pass in the context dictionary: we'll give it the key form and we'll pass in that actual form object 
+               
+                checked = True 
+                
+            
+            
+
+        return render(request,'templates/triples/formpage.html',{'form':form, 'checked': checked,'graph':graph }) #request,pass in th page i want to show,pass in the context dictionary: we'll give it the key form and we'll pass in that actual form object 
 
 
