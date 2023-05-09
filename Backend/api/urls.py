278adc447140b93ce58e0d20487993ca437de619
Matal1x <aslaoui.mohamed@gmail.com>
2023-05-09 21:06:28 +0100

    awaiting button fix

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -5,10 +5,23 @@ urlpatterns= [
     #path('get/SELECT/<slug:graph>/<str:subject>/<str:predicat>/<slug:objectV>/', views.api_get_SELECT_JSON, name='SELECT'),
     re_path(r'get/SELECT/(?P<graph>[\w-]+)/(?P<subject>[a-zA-Z0-9]+)/(?P<predicat>[a-zA-Z]+)/(?P<objectV>-?\d+(\.\d+)?|\w+(\.\w+)*)/$', views.api_get_SELECT_JSON, name='SELECT_re'),
     
-    path('get/ASK/<slug:graph>/<str:subject>/<str:predicat>/<slug:objectV>/', views.api_get_ASK, name='ASK'),
-    path('get/CONSTRUCT/<slug:graph>/<str:subject>/<str:predicat>/<slug:objectV>/', views.api_get_CONSTRUCT, name='CONSTRUCT'),
+    #path('get/ASK/<slug:graph>/<str:subject>/<str:predicat>/<slug:objectV>/', views.api_get_ASK, name='ASK'),
+    re_path(r'get/ASK/(?P<graph>[\w-]+)/(?P<subject>[a-zA-Z0-9]+)/(?P<predicat>[a-zA-Z]+)/(?P<objectV>-?\d+(\.\d+)?|\w+(\.\w+)*)/$', views.api_get_ASK, name='ASK_re'),
     
-    path('put/INSERT/<slug:graph>/<str:subject>/<str:predicat>/<slug:objectV>/', views.api_put_INSERT, name='INSERT'),
-    path('put/DELETE/<slug:graph>/<str:subject>/<str:predicat>/<slug:objectV>/', views.api_put_DELETE, name='DELETE'), 
-    path('put/MODIFY/<slug:graph>/<str:subject>/<str:predicat>/<slug:objectV>/', views.api_put_MODIFY, name='MODIFY'),
+    #path('get/CONSTRUCT/<slug:graph>/<str:subject>/<str:predicat>/<slug:objectV>/', views.api_get_CONSTRUCT, name='CONSTRUCT'),
+    re_path(r'get/CONSTRUCT/(?P<graph>[\w-]+)/(?P<subject>[a-zA-Z0-9]+)/(?P<predicat>[a-zA-Z]+)/(?P<objectV>-?\d+(\.\d+)?|\w+(\.\w+)*)/$', views.api_get_CONSTRUCT, name='CONSTRUCT_re'),
+    
+    
+    
+    
+    
+    
+    #path('put/INSERT/<slug:graph>/<str:subject>/<str:predicat>/<slug:objectV>/', views.api_put_INSERT, name='INSERT'),
+    re_path(r'put/INSERT/(?P<graph>[\w-]+)/(?P<subject>[a-zA-Z0-9]+)/(?P<predicat>[a-zA-Z]+)/(?P<objectV>-?\d+(\.\d+)?|\w+(\.\w+)*)/$', views.api_put_INSERT, name='INSERT_re'),
+    
+    #path('put/DELETE/<slug:graph>/<str:subject>/<str:predicat>/<slug:objectV>/', views.api_put_DELETE, name='DELETE'), 
+    re_path(r'put/DELETE/(?P<graph>[\w-]+)/(?P<subject>[a-zA-Z0-9]+)/(?P<predicat>[a-zA-Z]+)/(?P<objectV>-?\d+(\.\d+)?|\w+(\.\w+)*)/$', views.api_put_DELETE, name='DELETE_re'),
+    
+    #path('put/MODIFY/<slug:graph>/<str:subject>/<str:predicat>/<slug:objectV>/', views.api_put_MODIFY, name='MODIFY'),
+    re_path(r'put/MODIFY/(?P<graph>[\w-]+)/(?P<subject>[a-zA-Z0-9]+)/(?P<predicat>[a-zA-Z]+)/(?P<objectV>-?\d+(\.\d+)?|\w+(\.\w+)*)/$', views.api_put_MODIFY, name='MODIFY_re'),
 ]
